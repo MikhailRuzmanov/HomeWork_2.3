8e43f97726ec8686985251b1665e6f1e8c17b82b
MikhailRuzmanov <mihailruzmanov@mail.ru>
2023-09-26 12:40:39 +0300

    Сделано Домашнее задание №2.3 Исправление №4. Добавил метод service().

--- src/ru/skypro/HomeWork_2_3/Bicycle.java
+++ src/ru/skypro/HomeWork_2_3/Bicycle.java
@@ -1,6 +1,13 @@
 package ru.skypro.HomeWork_2_3;
 
 public class Bicycle extends WheeledVehicles {
+    public void service(){
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+
+
+    }
 
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
